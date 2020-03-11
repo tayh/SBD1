f054ce93d7e67e30dc87c20061c4a6c587916909
Taynara Carvalho <tayhcarvalho@gmail.com>
2020-03-11 11:35:47 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+from sys import exit
+
+def criar_registro(**kwargs):
+    codigo = kwargs.get('codigo')
+    nome = kwargs.get('nome')
+    data_nascimento = kwargs.get('data_nascimento')
+    cpf = kwargs.get('cpf')
+    endereco = kwargs.get('endereco')
+    placa = kwargs.get('placa')
+    modelo = kwargs.get('modelo')
+    ano = kwargs.get('ano')
+    cor = kwargs.get('cor')
+    
+    
+    registros = {
+        'registro' + str(codigo):{
+            'Pessoa': {'Nome': nome, 'Data de Nacimento': data_nascimento, 'CPF': cpf, 'Endereço': endereco},
+            'Automóvel': {'Placa': placa, 'Modelo': modelo, 'Ano': ano, 'Cor': cor},
+        }
+    }
+    
+    arquivo = open("arquivo.txt", "a")
+    arquivo.write(str(registros) + '\n')
+    arquivo.close()
+    return registros
+
+while True:
+    print('--------------------------')
+    print('O que deseja fazer ?')
+    print('1 - Inserir um registro')
+    print('2 - Editar um registro')
+    print('3 - Excluir um registro')
+    print('4 - Sair')
+    print('---------------------------')
+    action = input("\n> ")
+    if '1' in action:
+        criar_registro(
+            codigo=4, nome='Taynara', 
+            data_nascimento='12/09/1995', 
+            cpf='04930907110', endereco='Quadra 1 Lote 1700', 
+            placa='EJKC3', modelo='Captur', ano='2017', cor='pink')
+    elif '2' in action:
+        print('editar')
+    elif '3' in action:
+        print('excluir')
+    elif '4' in action:
+        exit(0)
+
